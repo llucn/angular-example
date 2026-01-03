9bc419372a05a01950090cba8c9dc1d645336561
Lane Lu <161492667+llucn@users.noreply.github.com>
2026-01-03 17:20:19 +0800

    feat: add cancel button

--- src/app/upload-file/upload-file.ts
+++ src/app/upload-file/upload-file.ts
@@ -1,51 +1,72 @@
-import { Component, signal, computed, inject } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule, HttpEventType } from '@angular/common/http';
+import { catchError, finalize, Subscription, tap, throwError } from 'rxjs';
 import { FormsModule } from '@angular/forms';
-import { Uploader } from '../service/uploader';
 
 @Component({
-  imports: [HttpClientModule, FormsModule],
+  imports: [HttpClientModule, FormsModule, CommonModule],
   selector: 'upload-file',
   templateUrl: './upload-file.html',
   styleUrl: './upload-file.scss',
 })
 export class UploadFile {
+  @Input()
+  requiredFileType: string = '';
   http = inject(HttpClient);
+  fileName = 'N/A';
+  subscription: Subscription | null = null;
   uploadProgress: number = 0;
-  value:number = 0;
   isUploading: boolean = false;
-  fileToUpload: File | null = null;
-  private uploader = inject(Uploader); 
 
   onFileSelected(event: any): void {
     const file: File = event.target.files[0];
     if (file) {
+      this.fileName = file.name;
       const formData = new FormData();
-      formData.append('file', file);
+      formData.append(file.name, file);
 
       this.isUploading = true;
-      this.http.post('/upload', formData, {
+      const upload$ = this.http.post('/upload', formData, {
         reportProgress: true,
         observe: 'events',
-      }).subscribe((event: any) => {
-        console.log(event)
+      }).pipe(
+        tap((event: any) => {
+          console.log('tap', event);
+        }),
+        catchError((error) => {
+          console.log(error);
+          return throwError(() => error);
+        }),
+        finalize(() => {
+          setTimeout(() => {
+            this.isUploading = false;
+          }, 200);
+        }),
+      );
+
+      this.subscription = upload$.subscribe((event: any) => {
+        console.log('subscription', event);
         if (event.type === HttpEventType.UploadProgress) {
           if (event.total) {
             this.uploadProgress = Math.round((100 * event.loaded) / event.total);
-            console.log("uploadProgress",this.uploadProgress);
+            console.log("uploadProgress", this.uploadProgress);
           }
         } else if (event.type === HttpEventType.Response) {
           console.log('Upload complete:', event.body);
           this.uploadProgress = 100;
-          setTimeout(() => {
-            this.isUploading = false;
-          }, 1000);
         }
-      },
-      (error: any) => {
-        console.error('Upload error:', error);
-        this.isUploading = false;
       });
     }
+  }
+
+  cancelUpload() {
+    this.subscription?.unsubscribe();
+    this.reset();
+  }
+
+  reset() {
+    this.uploadProgress = 0;
+    this.subscription = null;
   }
 }
